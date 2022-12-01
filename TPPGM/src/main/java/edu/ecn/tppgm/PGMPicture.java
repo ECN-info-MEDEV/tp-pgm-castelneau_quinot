4fe87bb518c35ed8ae566321bdbd94a04493eb8e
Erwan QUINOT <erwan.quinot@gmail.com>
2022-12-01 09:59:24 +0100

    added image

---
+++ TPPGM/src/main/java/edu/ecn/tppgm/PGMPicture.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ecn.tppgm;
+
+/**
+ *
+ * @author erwan
+ */
+public class PGMPicture {
+    
+}
